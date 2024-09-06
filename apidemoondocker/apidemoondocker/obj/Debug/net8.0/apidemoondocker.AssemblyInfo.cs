90ebe2c5652f3adc714e2022d4b421a9722f1b72
shiv <shiv291@gmail.com>
2024-09-06 23:35:25 +0530

    new project

--- apidemoondocker/apidemoondocker/obj/Debug/net8.0/apidemoondocker.AssemblyInfo.cs
+++ apidemoondocker/apidemoondocker/obj/Debug/net8.0/apidemoondocker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apidemoondocker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3c4c6819762f5041c124eecea6f8bde1aebfe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a738c0f905e3e0201192db7a410e0c17916d2327")]
 [assembly: System.Reflection.AssemblyProductAttribute("apidemoondocker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apidemoondocker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
